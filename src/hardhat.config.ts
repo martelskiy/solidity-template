8fccbb7ad9630a221a7f72df8e445cd8b883220c
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-09-27 08:56:20 +0200

    Skeleton

---
+++ src/hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: "0.8.21" }],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        enabled: false, //set to 'false' when API KEY env var is not set
+        url: ``,
+      },
+    },
+  },
+};
+
+export default config;
